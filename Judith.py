ed67dbb8a6cfb4b56acdf6772f190f5c42585339
Matheus <mfonse0@gmail.com>
2020-12-11 23:23:25 -0300

    V0.2
    Holofernes perdeu a cabeça de vez. VAI JUDITH!

---
+++ Judith.py
@@ -0,0 +1,71 @@
+######################################
+#       _           _ _ _   _
+#     (_)_   _  __| (_| |_| |__
+#    | | | | |/ _` | | __| '_ \
+#   | | |_| | (_| | | |_| | | |
+# _/ |\__,_|\__,_|_|\__|_| |_|
+#|__/
+#Make Holofernes pay with his head and then search 
+#your bagulho which is comming de jegue 
+#from the correio.
+#Matheu - 2020
+#Puta que pariu que ano desgraçado.
+#######################################
+
+#gonna use selenium to scrap the fuck out of this fucker
+#and import time & keys from it
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+#maximus variables of this shit
+#path is setted to edgedriver in this case, 
+#you'll gonna need to download the apropriate version to your browser tho
+#its not difficult to implement to another browser, just change the driver
+#and the correct appointment in the driver section to "webriver.Yourcorrectbrowser.exe"
+PATH = "..\\Judith\\msedgedriver.exe"
+
+#the correios website
+SITE = "https://www2.correios.com.br/sistemas/rastreamento/default.cfm"
+
+
+
+#main operations
+
+title = """
+   _           _ _ _   _
+  (_)_   _  __| (_| |_| |__
+  | | | | |/ _` | | __| '_ \\
+  | | |_| | (_| | | |_| | | |
+ _/ |\__,_|\__,_|_|\__|_| |_|
+|__/
+              0.2
+
+
+"""
+
+print(title)
+#the 13 digits tracking number
+print("Insira o código de 13 digitos: ")
+FUCKING_NUMBER = input(">> ")
+print("Procurando...")
+
+driver = webdriver.Edge(PATH)
+driver.get(SITE)
+
+#find the element by his name, it can be by its ID or CSS tho
+element = driver.find_element_by_name("objetos")
+element.clear()
+
+#insert the apropriate tracking code and then proceeds to the next page
+element.send_keys(FUCKING_NUMBER)
+element.send_keys(Keys.RETURN)
+
+#save a screenshot of the tracking progress bc im lazy right now to implement a callback terminal function
+driver.save_screenshot('consulta.png')
+
+#if the code is wrong, it'll show this output
+assert "Sem bagulhos encontrados, mano." not in driver.page_source
+
+#close the program and the browser window. BEWARE
+driver.close()
+exit()
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-Judith
+# Judith
+**_Holofernes nunca mais vai perder a cabeça com a demora para encontrar as suas encomendas._**
+
+Com a Judith você automatiza alguns processos para a pesquisa de um ou mais códigos de rastreio em questão de segundos e de quebra ainda fica com uma captura de tela da ultima atualização do transporte para consulta rápida.
+
+Esta versão usa o EDGE como motor principal e o código está totalmente otimizado para o mesmo, mas em meia dúzia de modificações funciona tanto com o firefox quanto com o chrome.
+
+### Como funciona?
+- Você precisa, antes de mais nada, possuir o Python 3 e o Selenium instalado.
+- Baixar o webdriver compativel com o seu browser em um dos links no fim do artigo.
+- Descompactar o driver na pasta da Judith.
+- Abrir o Judith.Py
+- Inserir os códigos e apertar enter.
+- Visualizar o Consulta.png na pasta da Judith.
+
+
+
+## Webdriver
+[Chrome](https://chromedriver.chromium.org/)
+[EDGE](https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/)
+[Firefox](https://developer.mozilla.org/en-US/docs/Web/WebDriver)
