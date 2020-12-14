8ee22a97f54e1487878c81d61a597477cc838543
Matheus <mfonse0@gmail.com>
2020-12-14 13:33:24 -0300

    V1.0
    Cleaned some code & started to implement headless

--- Judith.py
+++ Judith.py
@@ -11,67 +11,79 @@
 #Matheu - 2020
 #Puta que pariu que ano desgraçado.
 #######################################
-
 #gonna use selenium to scrap the fuck out of this fucker
 #and import time & keys from it
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.edge.options import Options
+from msedge.selenium_tools import Edge, EdgeOptions
+
+#######################################
 
 #maximus variables of this shit
 #path is setted to edgedriver in this case, 
 #you'll gonna need to download the apropriate version to your browser tho
 #its not difficult to implement to another browser, just change the driver
-
 #and the correct appointment in the driver section to "webriver.Yourcorrectbrowser.exe"
 PATH = "..\\Judith\\msedgedriver.exe"
-
 #read the tracking code(s) from the tracking txt file
 #you can put up various tracking numbers, but dont forget to separate them with commas.
 TRACKING = open('..\\Judith\\tracking.txt', 'r')
+#tracking number
 CODE = TRACKING.read()
-
 #the correios website
 SITE = "https://www2.correios.com.br/sistemas/rastreamento/default.cfm"
+#basic config
+options = EdgeOptions()                   #ebable the options or edge
+options.use_chromium = True               #uses chromium engine
+options.add_argument("headless")          #enable headless mode
+options.add_argument("disable-gpu")
+options.add_argument("log-level=3")       #disable warnings
 
+#######################################
 
+def judith(SITE):
+   driver = Edge(PATH)
+   driver.get(SITE)
+   #find the element by his name, it can be by its ID or CSS tho
+   element = driver.find_element_by_name("objetos")
+   element.clear()
+   #insert the apropriate tracking code and then proceeds to the next page
+   element.send_keys(CODE)
+   element.send_keys(Keys.RETURN)
+
+   #must implement total headless mode and print in terminal the state
+   #it needs to go here
+   #in this exact spot
+
+   #save a screenshot of the tracking progress bc im lazy right now to implement a callback terminal function
+   driver.save_screenshot('consulta.png')
+   #if the code is wrong, it'll show this output
+   assert "Sem bagulhos encontrados, mano." not in driver.page_source
+   #close the program and the browser window. BEWARE
+   driver.close()
+
+def title(CODE):
+   title = """
+      _           _ _ _   _
+     (_)_   _  __| (_| |_| |__
+     | | | | |/ _` | | __| '_ \\
+     | | |_| | (_| | | |_| | | |
+    _/ |\__,_|\__,_|_|\__|_| |_|
+   |__/
+                1.0
+   """
+   print(title)
+   #the 13 digits tracking number
+   print("Lendo o(s) códigos de rastreio.")
+   print("Código(s) >> ", CODE)
+   print("Procurando...")
+
+#######################################
 
 #main operations
+title(CODE)
 
-title = """
-   _           _ _ _   _
-  (_)_   _  __| (_| |_| |__
-  | | | | |/ _` | | __| '_ \\
-  | | |_| | (_| | | |_| | | |
- _/ |\__,_|\__,_|_|\__|_| |_|
-|__/
-              0.2
+judith(SITE)
 
-
-"""
-
-print(title)
-#the 13 digits tracking number
-print("Lendo o(s) códigos de rastreio.")
-print("Código(s) >> ", CODE)
-print("Procurando...")
-
-driver = webdriver.Edge(PATH)
-driver.get(SITE)
-
-#find the element by his name, it can be by its ID or CSS tho
-element = driver.find_element_by_name("objetos")
-element.clear()
-
-#insert the apropriate tracking code and then proceeds to the next page
-element.send_keys(CODE)
-element.send_keys(Keys.RETURN)
-
-#save a screenshot of the tracking progress bc im lazy right now to implement a callback terminal function
-driver.save_screenshot('consulta.png')
-
-#if the code is wrong, it'll show this output
-assert "Sem bagulhos encontrados, mano." not in driver.page_source
-
-#close the program and the browser window. BEWARE
-driver.close()
 exit()
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Com a Judith você automatiza alguns processos para a pesquisa de um ou mais có
 Esta versão usa o EDGE como motor principal e o código está totalmente otimizado para o mesmo, mas em meia dúzia de modificações funciona tanto com o firefox quanto com o chrome.
 
 ### Como funciona?
-- Você precisa, antes de mais nada, possuir o Python 3 e o Selenium instalado.
+- Você precisa, antes de mais nada, possuir o Python 3, o Edge Selenium Tools e o Selenium instalado.
 - Baixar o webdriver compativel com o seu browser em um dos links no fim do artigo.
 - Descompactar o driver na pasta da Judith.
 - Editar o arquivo tracking.txt com o código de rastreio de 13 digitos.
@@ -18,4 +18,5 @@ Esta versão usa o EDGE como motor principal e o código está totalmente otimiz
 ## Webdriver
 [Chrome](https://chromedriver.chromium.org/)
 [EDGE](https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/)
+[EDGE Selenium Tools](https://github.com/microsoft/edge-selenium-tools)
 [Firefox](https://developer.mozilla.org/en-US/docs/Web/WebDriver)
